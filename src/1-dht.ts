9b14b59b96c334abcbf0929ace66880d7cc937b5
ming2 <ming2@Mingders-Mac-mini.local>
2025-08-26 15:40:12 +0800

    npm init -y

---
+++ src/1-dht.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { bootstrap } from '@libp2p/bootstrap'
+import { identify } from '@libp2p/identify'
+import { kadDHT, removePublicAddressesMapper } from '@libp2p/kad-dht'
+import { ping } from '@libp2p/ping'
+import { tcp } from '@libp2p/tcp'
+import { createLibp2p } from 'libp2p'
+import bootstrappers from './bootstrappers.ts'
+
+const node = await createLibp2p({
+  addresses: {
+    listen: ['/ip4/0.0.0.0/tcp/0']
+  },
+  transports: [tcp()],
+  streamMuxers: [yamux()],
+  connectionEncrypters: [noise()],
+  peerDiscovery: [
+    bootstrap({
+      list: bootstrappers
+    })
+  ],
+  services: {
+    kadDHT: kadDHT({
+      protocol: '/ipfs/lan/kad/1.0.0',
+      peerInfoMapper: removePublicAddressesMapper,
+      clientMode: false
+    }),
+    identify: identify(),
+    ping: ping()
+  }
+})
+
+node.addEventListener('peer:connect', (evt) => {
+  const peerId = evt.detail
+  console.log('Connection established to:', peerId.toString()) // Emitted when a peer has been found
+})
+
+node.addEventListener('peer:discovery', (evt) => {
+  const peerInfo = evt.detail
+
+  console.log('Discovered:', peerInfo.id.toString())
+})
